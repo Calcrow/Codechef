cfa6ccfd5502b90cf6f5c145e0192960a0fcf247
dhruvs <dhruvsaini847@gmail.com>
2022-12-07 16:35:42 +0530

    Solved

---
+++ Expensive_Steps.py
@@ -0,0 +1,5 @@
+for  _ in range(int(input)):
+    n,x1,y1,x2,y2=map(int,input().split())
+    offgridcost=min(min((n+1-y1),y1),min((n+1-x1),x1))+min(min((n+1-x2),x2),min((n+1-y2),y2))
+    ingrigcost=abs(x1-x2)+abs(y1-y2)
+    print(min(oofgridcost,ingridcost))
